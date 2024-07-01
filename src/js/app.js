9266cc4033789fc1c6964e34bb8bc1f347f92288
giuliodalbono <dalbonogiulio@gmail.com>
2024-07-01 22:36:17 +0200

    Implemented scene setup

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,11 @@
 /**
  * Main js for application.
  */
+import { createScene } from "./scene.js";
+
+window.addEventListener('load', init, false);
+
+function init() {
+    // Set up the scene, the camera and the renderer
+    createScene();
+}
---
+++ src/js/scene.js
@@ -0,0 +1,64 @@
+import * as THREE from "../../vendors/three.module.js";
+
+let HEIGHT, WIDTH, camera, renderer;
+
+export function createScene() {
+    // Get the width and the height of the screen, use them to set up the aspect ratio of the camera and the size of the
+    // renderer
+    HEIGHT = window.innerHeight;
+    WIDTH = window.innerWidth;
+
+    // Create the scene
+    const scene = new THREE.Scene();
+
+    // Add a fog effect to the scene; same color as the background color used in the style sheet
+    scene.fog = new THREE.Fog(0xf7d9aa, 100, 950);
+
+    // Create the camera
+    const aspectRatio = WIDTH / HEIGHT;
+    const fieldOfView = 60;
+    const nearPlane = 1;
+    const farPlane = 10000;
+    camera = new THREE.PerspectiveCamera(
+        fieldOfView,
+        aspectRatio,
+        nearPlane,
+        farPlane
+    );
+
+    // Set the position of the camera
+    camera.position.x = 0;
+    camera.position.z = 200;
+    camera.position.y = 100;
+
+    // Create the renderer
+    renderer = new THREE.WebGLRenderer({
+        // Allow transparency to show the gradient background we defined in the CSS
+        alpha: true,
+
+        // Activate the antialiasing; this is less performant, but, as the project is low-poly based, it should be fine
+        antialias: true
+    });
+
+    // Define the size of the renderer; in this case, it will fill the entire screen
+    renderer.setSize(WIDTH, HEIGHT);
+
+    // Enable shadow rendering
+    renderer.shadowMap.enabled = true;
+
+    // Add the DOM element of the renderer to the container we created in the HTML
+    const container = document.getElementById('world');
+    container.appendChild(renderer.domElement);
+
+    // Listen to the screen: if the user resizes it we have to update the camera and the renderer size
+    window.addEventListener('resize', handleWindowResize, false);
+}
+
+function handleWindowResize() {
+    // Update height and width of the renderer and the camera
+    HEIGHT = window.innerHeight;
+    WIDTH = window.innerWidth;
+    renderer.setSize(WIDTH, HEIGHT);
+    camera.aspect = WIDTH / HEIGHT;
+    camera.updateProjectionMatrix();
+}
