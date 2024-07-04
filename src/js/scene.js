ef71946eb555c43dc8139109df42b4a921da08e4
giuliodalbono <dalbonogiulio@gmail.com>
2024-07-04 16:11:23 +0200

    Fixed scene

--- src/js/scene.js
+++ src/js/scene.js
@@ -1,7 +1,6 @@
 import * as THREE from "../../vendors/three.module.js";
 
-export let scene;
-let HEIGHT, WIDTH, camera, renderer;
+let HEIGHT, WIDTH;
 
 export function createScene() {
     // Get the width and the height of the screen, use them to set up the aspect ratio of the camera and the size of the
@@ -10,22 +9,20 @@ export function createScene() {
     WIDTH = window.innerWidth;
 
     // Create the scene
-    scene = new THREE.Scene();
-
-    // Add a fog effect to the scene; same color as the background color used in the style sheet
-    scene.fog = new THREE.Fog(0xf7d9aa, 100, 950);
+    window['scene'] = new THREE.Scene();
 
     // Create the camera
     const aspectRatio = WIDTH / HEIGHT;
     const fieldOfView = 60;
     const nearPlane = 1;
     const farPlane = 10000;
-    camera = new THREE.PerspectiveCamera(
+    const camera = new THREE.PerspectiveCamera(
         fieldOfView,
         aspectRatio,
         nearPlane,
         farPlane
     );
+    window['camera'] = camera;
 
     // Set the position of the camera
     camera.position.x = 0;
@@ -33,13 +30,14 @@ export function createScene() {
     camera.position.y = 100;
 
     // Create the renderer
-    renderer = new THREE.WebGLRenderer({
+    const renderer = new THREE.WebGLRenderer({
         // Allow transparency to show the gradient background we defined in the CSS
         alpha: true,
 
         // Activate the antialiasing; this is less performant, but, as the project is low-poly based, it should be fine
         antialias: true
     });
+    window['renderer'] = renderer;
 
     // Define the size of the renderer; in this case, it will fill the entire screen
     renderer.setSize(WIDTH, HEIGHT);
