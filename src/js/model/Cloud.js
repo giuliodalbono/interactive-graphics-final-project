66dbf6847408272cdf23c96a9867ccc22cda8ca1
giuliodalbono <dalbonogiulio@gmail.com>
2024-07-04 16:14:18 +0200

    Created cloud

---
+++ src/js/model/Cloud.js
@@ -0,0 +1,42 @@
+import * as THREE from "../../../vendors/three.module.js";
+import { Colors } from "../color.js";
+
+export class Cloud {
+    constructor() {
+        // Create an empty container that will hold the different parts of the cloud
+        this.mesh = new THREE.Object3D();
+
+        // Create a cube geometry;
+        // this shape will be duplicated to create the cloud
+        const geometry = new THREE.BoxGeometry(20, 20, 20);
+
+        // Create a material; a simple white material will do the trick
+        const material = new THREE.MeshPhongMaterial({ color: Colors.white });
+
+        // Duplicate the geometry a random number of times
+        const nBlocs = 3 + Math.floor(Math.random() * 3);
+        for (let i = 0; i < nBlocs; i++ ) {
+
+            // Create the mesh by cloning the geometry
+            const m = new THREE.Mesh(geometry, material);
+
+            // Set the position and the rotation of each cube randomly
+            m.position.x = i * 15;
+            m.position.y = Math.random() * 10;
+            m.position.z = Math.random() * 10;
+            m.rotation.z = Math.random() * Math.PI * 2;
+            m.rotation.y = Math.random() * Math.PI * 2;
+
+            // Set the size of the cube randomly
+            const s = .1 + Math.random() * .9;
+            m.scale.set(s,s,s);
+
+            // Allow each cube to cast and to receive shadows
+            m.castShadow = true;
+            m.receiveShadow = true;
+
+            // add the cube to the container we first created
+            this.mesh.add(m);
+        }
+    }
+}
